a58ee1064098d0421ee20b7facb141830e040d97
programakc <veryneed.email@gmail.com>
2025-09-17 16:06:17 +0200

    pattern for certification

---
+++ src/certification/certification01/model/User.java
@@ -0,0 +1,14 @@
+package certification.certification01.model;
+
+public class User {
+
+    private String id;
+    private String login;
+    private String password;
+    private String confirmPassword;
+    private String surname;
+    private String firstName;
+    private String middleName;
+    private Integer age;
+    private Boolean isWorker;
+}
---
+++ src/certification/certification01/repositories/UsersRepositoryFileImpl.java
@@ -0,0 +1,37 @@
+package certification.certification01.repositories;
+
+import certification.certification01.model.User;
+
+import java.util.List;
+
+public class UsersRepositoryFileImpl implements UsersRepositories{
+    @Override
+    public void create(User user) {
+
+    }
+
+    @Override
+    public User findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void deleteById(String id) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
