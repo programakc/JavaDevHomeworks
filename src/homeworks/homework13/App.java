930e2422a30f02d3a14299d7083778c26748707f
programakc <veryneed.email@gmail.com>
2025-09-30 12:11:50 +0200

    completing the main task

--- src/homeworks/homework13/App.java
+++ src/homeworks/homework13/App.java
@@ -1,9 +1,17 @@
 package homeworks.homework13;
 
+import homeworks.homework13.utils.DataProcessing;
+
 public class App {
 
     public static void main(String[] args) {
 
+        Integer test1 = DataProcessing.parseCount("123");
 
+        System.out.println(test1);
+
+        Integer test2 = DataProcessing.validateCount("Проверка");
+
+        System.out.println(test2);
     }
 }
--- src/homeworks/homework13/utils/DataProcessing.java
+++ src/homeworks/homework13/utils/DataProcessing.java
@@ -1,4 +1,22 @@
 package homeworks.homework13.utils;
 
+import java.sql.SQLOutput;
+
 public class DataProcessing {
+
+    public static Integer parseCount(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Невалидное значение");
+        }
+    }
+
+    public static Integer validateCount(String str) {
+        try {
+            return parseCount(str);
+        } catch (RuntimeException e) {
+            throw e;
+        }
+    }
 }
