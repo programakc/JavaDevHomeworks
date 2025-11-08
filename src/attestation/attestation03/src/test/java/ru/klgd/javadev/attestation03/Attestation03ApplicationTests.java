1d2e78d065c845d96a7271c6d7b84e63c60d7b19
programakc <veryneed.email@gmail.com>
2025-11-08 12:12:00 +0200

    create attestation03 template

---
+++ src/attestation/attestation03/src/test/java/ru/klgd/javadev/attestation03/Attestation03ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.klgd.javadev.attestation03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Attestation03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
