523fccc28f1d9a2ea0f5bf8568a02c9830cfeffd
programakc <veryneed.email@gmail.com>
2025-11-08 11:57:59 +0200

    start homework19

---
+++ src/homeworks/homework19/src/test/java/ru/klgd/javadev/homework19/Homework19ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.klgd.javadev.homework19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
