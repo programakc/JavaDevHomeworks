b7cc3ff1e60566deeb1b56dcacb52a7cfc8a0372
programakc <veryneed.email@gmail.com>
2025-11-23 00:20:57 +0200

    Описание веб-контроллера с помощью аннотаций Swagger UI

--- src/homeworks/homework19/src/main/java/ru/klgd/javadev/homework19/controllers/TelevisionController.java
+++ src/homeworks/homework19/src/main/java/ru/klgd/javadev/homework19/controllers/TelevisionController.java
@@ -1,5 +1,7 @@
 package ru.klgd.javadev.homework19.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,9 @@ public class TelevisionController {
 
     private final TelevisionServiceImpl televisionService;
 
+    @Operation(summary = "Создание новой записи в таблице televisions",
+            description = "В ответе возвращается объект TelevisionResponse c " +
+                    "полями company, model, release, price и countInStock.")
     @PostMapping
     public ResponseEntity<TelevisionResponse> createTelevision(@RequestBody TelevisionRequest televisionRequest) {
         TelevisionResponse newTelevision = televisionService.createTelevision(televisionRequest);
@@ -22,23 +27,38 @@ public class TelevisionController {
         return new ResponseEntity<>(newTelevision, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Поиск телевизора по ID",
+            description = "Если телевизор найден, то в ответе возвращается " +
+                    "объект TelevisionResponse c полями company, model, release, price и countInStock.")
     @GetMapping("/{id}")
-    public ResponseEntity<TelevisionResponse> getTelevisionById(@PathVariable Long id) {
+    public ResponseEntity<TelevisionResponse> getTelevisionById(
+            @Parameter(description = "ID телевизора, данные по которому запрашиваются", required = true)
+            @PathVariable Long id) {
+
         TelevisionResponse television = televisionService.getTelevisionById(id);
 
         return new ResponseEntity<>(television, HttpStatus.OK);
     }
 
+    @Operation(summary = "Обновление данных о телевизоре, найденному по ID",
+            description = "Если телевизор найден, то в ответе возвращается " +
+                    "объект TelevisionResponse c обновлёнными полями company, model, release, price и countInStock.")
     @PutMapping("/{id}")
-    public ResponseEntity<TelevisionResponse> updateTelevision(@PathVariable Long id,
-                                                   @RequestBody TelevisionRequest televisionRequest){
+    public ResponseEntity<TelevisionResponse> updateTelevision(
+            @Parameter(description = "ID телевизора, данные которого необходимо обновить", required = true)
+            @PathVariable Long id, @RequestBody TelevisionRequest televisionRequest){
 
         TelevisionResponse updatedTelevision = televisionService.updateTelevision(id, televisionRequest);
         return new ResponseEntity<>(updatedTelevision, HttpStatus.OK);
     }
 
+    @Operation(summary = "Обновление данных о телевизоре, найденному по ID",
+            description = "В ответе возвращается пустое тело запроса")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTelevision(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteTelevision(
+            @Parameter(description = "ID телевизора, который требуется удалить", required = true)
+            @PathVariable Long id) {
+
         televisionService.deleteTelevision(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
