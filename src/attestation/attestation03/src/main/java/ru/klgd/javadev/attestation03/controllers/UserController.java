9a514f8eacc70ec98eb0eb97bf4a4011e4ae21d1
programakc <veryneed.email@gmail.com>
2025-11-30 10:52:42 +0200

    Выполнение пунктов итоговой аттестации

--- src/attestation/attestation03/src/main/java/ru/klgd/javadev/attestation03/controllers/UserController.java
+++ src/attestation/attestation03/src/main/java/ru/klgd/javadev/attestation03/controllers/UserController.java
@@ -1,5 +1,7 @@
 package ru.klgd.javadev.attestation03.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,9 @@ public class UserController {
 
     private final UserServiceImpl userService;
 
+    @Operation(summary = "Создание нового пользователя",
+            description = "В ответе возвращается объект UserResponse, в базу " +
+                    "данных пользователей добавлен новый элемент")
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest) {
         UserResponse newUser = userService.createUser(userRequest);
@@ -22,23 +27,39 @@ public class UserController {
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Получение данных пользователя по ID",
+            description = "В ответе возвращается объект UserResponse, " +
+                    "содержащий данные пользователя, найденного по ID")
     @GetMapping("/{id}")
-    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+    public ResponseEntity<UserResponse> getUserById(
+            @Parameter(description = "ID пользователя, которого ищем", required = true)
+            @PathVariable Long id) {
+
         UserResponse user = userService.getUserById(id);
 
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
+    @Operation(summary = "Обновление данных пользователя, найденного по ID",
+            description = "В ответе возвращается объект UserResponse, " +
+                    "содержащий обновлённые данные пользователя, найденного по ID")
     @PutMapping("/{id}")
-    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id,
-                                              @RequestBody UserRequest userRequest){
+    public ResponseEntity<UserResponse> updateUser(
+            @Parameter(description = "ID пользователя, данные которого необходимо обновить", required = true)
+            @PathVariable Long id,
+            @RequestBody UserRequest userRequest){
 
         UserResponse updatedUser = userService.updateUser(id, userRequest);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
+    @Operation(summary = "Удаление пользователя, найденного по ID",
+            description = "В ответе возвращается пустое тело запроса")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteUser(
+            @Parameter(description = "ID пользователя, которого необходимо удалить", required = true)
+            @PathVariable Long id) {
+
         userService.deleteUser(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
